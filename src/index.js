c9690c14a301aef1343998716836e116bf65cf5f
ShanhinPavel <36544060+ShanhinPavel@users.noreply.github.com>
2018-02-22 17:26:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,14 +6,19 @@ module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
   let count = 0;
 
+  for (let i = 0; i < preferences.length; i++) {
+    //to know whom does love first
+    
+let connect1 = preferences[i] - 1; // 1 we got link on
+    
+let connect2 = preferences[connect1] - 1; //2 we got link on
 
-for (var i = 0; i < preferences.length; i++){
-  let pre = preferences[preferences[preferences[i]]];
-  let pre1 = preferences[i + 1];
+let connect3 = preferences[connect2] - 1; //0 we got link on
 
-if (pre === pre1) {
-  count++;
+
+if (i === connect3){
+  count = count + 1;
 }
 }
-return count / 2;
+return Math.round(count / 3);
 };
